3ff73713997264533ce34ed10197c9831b63a55d
Eric Wyne <ecwyne@gmail.com>
2019-07-18 00:10:36 -0600

    initial typescript implementation

---
+++ src/fauna-graphql-client.ts
@@ -0,0 +1,28 @@
+import { Client, query as q } from "faunadb";
+import graphqlQueryFields from "graphql-fields";
+
+import { createTopLevelCompiler, DataModel } from "./fauna-compilers";
+import { GraphQLFieldResolver } from "graphql";
+
+class FaunaGraphQLClient {
+  constructor(private client: Client) {}
+
+  createRootResolver(models: DataModel, className: string, indexName: string) {
+    const compiler = createTopLevelCompiler(models, className);
+
+    const resolver: GraphQLFieldResolver<any, any, any> = (
+      _root,
+      _args,
+      _ctx,
+      info
+    ) => {
+      const refs = q.Paginate(q.Match(q.Index(indexName)));
+      const compiledQuery = compiler(graphqlQueryFields(info))(refs);
+      return this.client.query(compiledQuery);
+    };
+
+    return resolver;
+  }
+}
+
+module.exports = FaunaGraphQLClient;
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export * from "./fauna-compilers";
+export * from "./fauna-graphql-client";
